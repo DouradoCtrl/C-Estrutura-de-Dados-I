a0849a4aae99659878dc60135663905827b0ae64
DouradoCtrl <gdouradoestudante@gmail.com>
2024-05-27 17:24:57 -0300

    Conceitos dos ponteiros

--- Ponteiros/p001.c
+++ Ponteiros/p001.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 
 int main () {
-    int x = 18;
-    printf("O endereço da variável é %d", &x);
+    int x = 19;
+    int *ptr;
+
+    ptr = &x;
+
+    printf("O conteúdo da variável x = %d\n", x);
+    printf("O endereço da variável x = %p\n", &x);
+    printf("O conteúdo apontado ptr  = %d\n", *ptr);
+    printf("O endereço apontado ptr  = %p\n", ptr);
+    printf("O endereço do ptr        = %p\n", &ptr);
+
     return 0;
 }
+
+//Ponteiros
+// *ptr = O apontado por, conteúdo do endereço da variável que ptr aponta
+// ptr = endereço da variável
+// &ptr = endereço do ptr
