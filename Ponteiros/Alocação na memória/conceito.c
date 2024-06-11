5df815643a005179f6abc6e50ac273940191ff70
DouradoCtrl <gdouradoestudante@gmail.com>
2024-06-11 20:26:12 -0300

    update study

--- Ponteiros/Alocação na memória/conceito.c
+++ Ponteiros/Alocação na memória/conceito.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 
 int main () {
-    int *ptr;
-    printf("Endereco: %p\n", ptr);
+    int *ptr, x=30;
+    printf("Consulta de ptr sem malloc\n");
+    printf("Endereco: %p \n\n", ptr);
+
+    printf("Consulta X");
+    printf("Endereco: %p \nValor: %d\n\n", &x, x);
+    
+    printf("Consulta ponteiro com malloc aleatorio");
+    ptr = (int *) malloc(sizeof (int));
+    printf("Endereco: %p \nValor: %d\n\n", ptr, *ptr);
+
+
+    *ptr = 10;
+    printf("Consulta com a atribuicao no valor do ponteiro");
+    printf("Endereco: %p \nValor: %d\n\n", ptr, *ptr);
+
+    ptr = &x;
+    printf("Consulta com endereco de x sendo passado para o ponteiro");
+    printf("Endereco: %p \nValor: %d\n\n", ptr, *ptr);
+
+    //o endereço original do ponteiro se perdeu
+
+    printf("Consulta de um novo malloc para ptr");
+    ptr = (int *) malloc(sizeof (int));
+    printf("\nEndereco: %p \nValor: %d\n", ptr, *ptr);
+
 }
+
+//Usamos malloc para armazenar memoria suficiente para um vetor
---
+++ Ponteiros/Alocação na memória/conceitosemmalloc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int *ptr, x=30;
+    printf("Consulta de ptr sem malloc\n");
+    printf("Endereco: %p \n\n", ptr);
+
+}
